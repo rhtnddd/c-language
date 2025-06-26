2bdc8913a669da9e37f6ff6ecf312ee04fef1b1a
성민규 <qlalfql123@gmail.com>
2025-06-26 23:22:24 +0900

    현우문제

--- 포인터/poni.c
+++ 포인터/poni.c
@@ -1,8 +1,20 @@
+// #include <stdio.h>
+// int main(void)
+// {
+//     int a=10;
+//     int *p=&a;
+//     printf("%d\n",*p);
+//     printf("%p",p);
+// }
 #include <stdio.h>
-int main(void)
+int main()
 {
-    int a=10;
-    int *p=&a;
-    printf("%d\n",*p);
-    printf("%p",p);
+    char *arr[]={"fire","wind","earth","water","void"};
+    char **p[]={arr+3,arr+1,arr+4,arr,arr+2};
+    char ***pp = p+1;
+    printf("%s\n",**pp);
+    printf("%s\n",*(*++pp -1));
+    printf("%c\n",*(*(*pp+1)+1));
+    printf("%c\n",*(*(*(pp-2)+1)+2));
+    return 0;
 }
