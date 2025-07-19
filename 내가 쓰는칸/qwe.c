a50c61dd8cdfde22ee53ce42f6910c68473558e8
성민규 <qlalfql123@gmail.com>
2025-07-19 23:58:04 +0900

    백준 33964

--- 내가 쓰는칸/qwe.c
+++ 내가 쓰는칸/qwe.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
-int main(void)
-{
-    int n;
-    scanf("%d",&n);
-    for(int i=n;i>=0;i--)
-    {
-        printf("%d\n",i);
+#include <stdio.h>
+int main() {
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    int num1 = 0, num2 = 0;
+    for (int i = 0; i < X; i++) {
+        num1 = num1 * 10 + 1;
     }
+    for (int i = 0; i < Y; i++) {
+        num2 = num2 * 10 + 1;
+    }
+    printf("%d\n", num1 + num2);
+    return 0;
 }
