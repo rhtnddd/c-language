b6f9914071d00ada7d22d8e3cb4e6c6360ee2d5c
성민규 <qlalfql123@gmail.com>
2025-07-23 23:54:17 +0900

    백준 문제풀기

--- 내가 쓰는칸/ㅁㄴㅇ.c
+++ 내가 쓰는칸/ㅁㄴㅇ.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-int n[1500000]={0,1,};
 int main(void)
 {
-    int i;
-    long long int a;
-    scanf("%lld",&a);
-    int p=a%1500000;
-    for(i=2;i<=p;i++)
+    int a,sum=0;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++)
     {
-        n[i]=(n[i-1]+n[i-2])%1000000;
+        sum+=i;
     }
-    printf("%d",n[p]);
+    printf("%d",sum);
 }
