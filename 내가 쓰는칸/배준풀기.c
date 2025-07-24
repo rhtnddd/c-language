130234701cf5e7146d69190bcc1ab667d1f322e9
성민규 <qlalfql123@gmail.com>
2025-07-25 01:24:44 +0900

    백준 정렬하기 2

--- 내가 쓰는칸/배준풀기.c
+++ 내가 쓰는칸/배준풀기.c
@@ -1,28 +1,88 @@
+// #include <stdio.h>
+// int main(void)
+// {
+//     int e,s,m;
+//     int sum=1;
+//     scanf("%d %d %d",&e,&s,&m);
+//     int ee=1,ss=1,mm=1;
+//     while(!(ee==e&&ss==s&&mm==m))
+//     {
+//         ee++;
+//         ss++;
+//         mm++;
+//         sum++;
+//         if(ee>15)
+//         {
+//             ee=1;
+//         }
+//         if(ss>28)
+//         {
+//             ss=1;
+//         }
+//         if(mm>19)
+//         {
+//             mm=1;
+//         }
+//     }
+//     printf("%d",sum);
+// }
 #include <stdio.h>
-int main(void)
+int a[1000001];
+int temp_a[1000001];
+void merge(int *a, int start,int middle,int end)
 {
-    int e,s,m;
-    int sum=1;
-    scanf("%d %d %d",&e,&s,&m);
-    int ee=1,ss=1,mm=1;
-    while(!(ee==e&&ss==s&&mm==m))
+    int i=start;
+    int j=middle+1;
+    int k=start;
+    while(i<=middle&&j<=end)
     {
-        ee++;
-        ss++;
-        mm++;
-        sum++;
-        if(ee>15)
+        if(a[i]<=a[j])
         {
-            ee=1;
+            temp_a[k]=a[i++];
         }
-        if(ss>28)
-        {
-            ss=1;
+        else{
+            temp_a[k]=a[j++];
         }
-        if(mm>19)
+        k++;
+    }
+    if(i>middle){
+        for(int t=j;t<=end;++t)
         {
-            mm=1;
+            temp_a[k]=a[t];
+            ++k;
         }
     }
-    printf("%d",sum);
+	else{
+		for(int t=i;t<=middle;++t){
+			temp_a[k] = a[t];
+			++k;
+		}
+	}
+	for(int t=start;t<=end;++t){
+		a[t] = temp_a[t];
+	}
+}
+void mergeSort(int *a,int start,int end)
+{
+    if(start<end){
+        int middle=(start+end)/2;
+        mergeSort(a,start,middle);
+        mergeSort(a, middle+1, end);
+        merge(a, start, middle, end);
+    }
+}
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    mergeSort(a,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    return 0;
 }
