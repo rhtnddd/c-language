3e14179de75aab551bb89dc2a0f3f5dfaca24bc0
성민규 <qlalfql123@gmail.com>
2025-07-28 23:21:57 +0900

    백준 문제1676

--- 내가 쓰는칸/배준풀기.c
+++ 내가 쓰는칸/배준풀기.c
@@ -86,18 +86,87 @@
 //     }
 //     return 0;
 // }
+// #include <stdio.h>
+// int main() {
+//     int A, B, N;
+//     scanf("%d %d %d", &A, &B, &N);
+//     A %= B; 
+//     for (int i = 0; i < N; i++) {
+//         A *= 10;
+//         if (i == N - 1) {
+//             printf("%d\n", A / B);
+//             break;
+//         }
+//         A %= B;
+//     }
+//     return 0;
+// }
+// #include <stdio.h>
+// int custom_strlen(const char* str)
+// {
+//     int length = 0;
+//     while (str[length] != '\0')
+//     {
+//         length++;
+//     }
+//     return length;
+// }
+// int main(void)
+// {
+//     char word[101];
+//     scanf("%s", word);
+//     int count = 0;
+//     int len = custom_strlen(word);
+//     for (int i = 0; i < len; i++)
+//     {
+//         count++;
+//         if (word[i] == 'c')
+//         {
+//             if (i + 1 < len && (word[i+1] == '=' || word[i+1] == '-'))
+//             {
+//                 i++;
+//             }
+//         } else if (word[i] == 'd')
+//         {
+//             if (i + 1 < len && word[i+1] == 'z')
+//             {
+//                 if (i + 2 < len && word[i+2] == '=')
+//                 {
+//                     i += 2;
+//                 }
+//             }
+//             else if (i + 1 < len && word[i+1] == '-')
+//             {
+//                 i++;
+//             }
+//         }
+//         else if (word[i] == 'l' || word[i] == 'n')
+//         {
+//             if (i + 1 < len && word[i+1] == 'j')
+//             {
+//                 i++;
+//             }
+//         }
+//         else if (word[i] == 's' || word[i] == 'z')
+//         {
+//             if (i + 1 < len && word[i+1] == '=')
+//             {
+//                 i++;
+//             }
+//         }
+//     }
+//     printf("%d\n", count);
+//     return 0;
+// }
 #include <stdio.h>
-int main() {
-    int A, B, N;
-    scanf("%d %d %d", &A, &B, &N);
-    A %= B; 
-    for (int i = 0; i < N; i++) {
-        A *= 10;
-        if (i == N - 1) {
-            printf("%d\n", A / B);
-            break;
-        }
-        A %= B;
+int main(void)
+{
+    int n;
+    int sum=0;
+    scanf("%d",&n);
+    for(int i=5;i<=n;i*=5)
+    {
+        sum+=n/i;
     }
-    return 0;
+    printf("%d",sum);
 }
