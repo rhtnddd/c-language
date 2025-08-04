db42753c601971d21a84d6a9ce34492cd89ed0e3
성민규 <qlalfql123@gmail.com>
2025-08-04 19:50:31 +0900

    백준 1920문제

--- 내가 쓰는칸/배준풀기2.c
+++ 내가 쓰는칸/배준풀기2.c
@@ -78,63 +78,157 @@
 //     return 0;
 // }
 // 10828문제
+// #include <stdio.h>
+// #include <string.h>
+// int main(void)
+// {
+//     int n;
+//     int first=-1;
+//     int size=0;
+//     char s[10];
+//     int sum[100000];
+//     scanf("%d", &n);
+//     for (int i=0;i<n;i++)
+//     {
+//         scanf("%s",s);
+//         if (strcmp(s,"push")==0)
+//         {
+//             int a;
+//             scanf("%d",&a);
+//             sum[++first]=a;
+//             size++;
+//         }
+//         else if (strcmp(s,"pop")==0)
+//         {
+//             if (size==0)
+//             {
+//                 printf("-1\n");
+//             }
+//             else
+//             {
+//                 printf("%d\n",sum[first--]);
+//                 size--;
+//             }
+//         }
+//         else if (strcmp(s,"size")==0)
+//         {
+//             printf("%d\n",size);
+//         }
+//         else if (strcmp(s,"empty")==0)
+//         {
+//             if (size==0)
+//             {
+//                 printf("1\n");
+//             }
+//             else
+//             {
+//                 printf("0\n");
+//             }
+//         }
+//         else if (strcmp(s,"top")==0)
+//         {
+//             if (size==0)
+//             {
+//                 printf("-1\n");
+//             }
+//             else
+//             {
+//                 printf("%d\n",sum[first]);
+//             }
+//         }
+//     }
+// }
+// 1920문제
 #include <stdio.h>
-#include <string.h>
+int M[100001];
+int temp[100001];
+void merge(int *M, int start, int middle, int end){
+    int i=start;
+    int j=middle + 1;
+    int k=start;
+    while(i<=middle && j<=end)
+    {
+        if(M[i]<=M[j])
+        {
+            temp[k]=M[i++];
+        }
+        else
+        {
+            temp[k]=M[j++];
+        }
+        k++;
+    }
+    if(i>middle)
+    {
+        for(int t=j;t<=end;++t)
+        {
+            temp[k]=M[t];
+            ++k;
+        }
+    }
+    else
+    {
+        for(int t=i;t<=middle;++t)
+        {
+            temp[k]=M[t];
+            ++k;
+        }
+    }
+    for(int t=start;t<=end;++t)
+    {
+        M[t] = temp[t];
+    }
+}
+
+void mergeSort(int *M, int start, int end)
+{
+    if(start<end)
+    {
+        int middle=(start+end)/2;
+        mergeSort(M,start,middle);
+        mergeSort(M,middle+1,end);
+        merge(M,start,middle,end);
+    }
+}
+
+int binary_search(int M[], int count,int answer)
+{
+    int str = 0;
+    int end = count-1;
+    while(str<=end)
+    {
+        int mid=(str+end)/2;
+        if(M[mid]==answer)
+        {
+            return 1;
+        }
+        else if(M[mid]>answer)
+        {
+            end=mid-1;
+        }
+        else
+        {
+            str=mid+1;
+        }
+    }
+    return 0;
+}
+
 int main(void)
 {
     int n;
-    int first=-1;
-    int size=0;
-    char s[10];
-    int sum[100000];
-    scanf("%d", &n);
-    for (int i=0;i<n;i++)
+    int n1;
+    int ebuntamsec;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
     {
-        scanf("%s",s);
-        if (strcmp(s,"push")==0)
-        {
-            int a;
-            scanf("%d",&a);
-            sum[++first]=a;
-            size++;
-        }
-        else if (strcmp(s,"pop")==0)
-        {
-            if (size==0)
-            {
-                printf("-1\n");
-            }
-            else
-            {
-                printf("%d\n",sum[first--]);
-                size--;
-            }
-        }
-        else if (strcmp(s,"size")==0)
-        {
-            printf("%d\n",size);
-        }
-        else if (strcmp(s,"empty")==0)
-        {
-            if (size==0)
-            {
-                printf("1\n");
-            }
-            else
-            {
-                printf("0\n");
-            }
-        }
-        else if (strcmp(s,"top")==0)
-        {
-            if (size==0)
-            {
-                printf("-1\n");
-            }
-            else
-            {
-                printf("%d\n",sum[first]);
-            }
-        }
+        scanf("%d",&M[i]);
+    }
+    mergeSort(M,0,n-1);
+    scanf("%d",&n1);
+    for(int i=0;i<n1;i++)
+    {
+        scanf("%d",&ebuntamsec);
+        printf("%d\n",binary_search(M,n,ebuntamsec));
     }
 }
