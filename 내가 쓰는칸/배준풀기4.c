7748ec52f080eba6ffc68d27d54eeb0de79a0f77
성민규 <qlalfql123@gmail.com>
2025-08-15 01:20:18 +0900

    백준 1629문제

--- 내가 쓰는칸/배준풀기4.c
+++ 내가 쓰는칸/배준풀기4.c
@@ -14,16 +14,35 @@
 //     return 0;
 // }
 // 11727문제
+// #include <stdio.h>
+// int main(void)
+// {
+//     int a[1001]={1,1,};
+//     int n;
+//     scanf("%d",&n);
+//     for(int i=2;i<=n;i++)
+//     {
+//         a[i]=(a[i-1]+2*a[i-2])%10007;
+//     }
+//     printf("%d\n",a[n]);
+//     return 0;
+// }
+// 1629문제
 #include <stdio.h>
 int main(void)
 {
-    int a[1001]={1,1,};
-    int n;
-    scanf("%d",&n);
-    for(int i=2;i<=n;i++)
+    long long int a,b,c;
+    int sum=1;
+    scanf("%lld %lld %lld",&a,&b,&c);
+    while(b>0)
     {
-        a[i]=(a[i-1]+2*a[i-2])%10007;
+        if(b%2==1)
+        {
+            sum=(sum*a)%c;
+        }
+        b/=2;
+        a=(a*a)%c;
     }
-    printf("%d\n",a[n]);
+    printf("%d\n",sum);
     return 0;
 }
