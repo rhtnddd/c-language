736d5b03c38cf8547c0b57ce2325a67fcc1317c5
성민규 <qlalfql123@gmail.com>
2025-08-23 23:50:27 +0900

    배준	 1026문제

--- 내가 쓰는칸/백준 실버풀기.c
+++ 내가 쓰는칸/백준 실버풀기.c
@@ -1,2 +1,82 @@
-//15549문제
+// 15549문제
 // int -2147483648
+
+// 1158문제
+// #include <stdio.h>
+// int main(void)
+// {
+//     int N, K;
+//     if (scanf("%d %d", &N, &K) != 2) return 0;
+//     int q[5000];
+//     int head = 0, tail = 0, cnt = 0;
+//     for (int i = 1; i <= N; ++i) {
+//         q[tail] = i;
+//         tail = (tail + 1) % 5000;
+//         cnt++;
+//     }
+//     printf("<");
+//     while (cnt > 0) {
+//         for (int i = 0; i < K - 1; ++i) {
+//             int x = q[head];
+//             head = (head + 1) % 5000;
+//             q[tail] = x;
+//             tail = (tail + 1) % 5000;
+//         }
+//         int removed = q[head];
+//         head = (head + 1) % 5000;
+//         cnt--;
+
+//         if (cnt == 0) {
+//             printf("%d", removed);
+//         } else {
+//             printf("%d, ", removed);
+//         }
+//     }
+//     printf(">\n");
+//     return 0;
+// }
+
+// 1026문제
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    int temp=0;
+    int temp1=0;
+    int sum=0;
+    int a[100];
+    int b[100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+            if(b[i]<b[j])
+            {
+                temp1=b[i];
+                b[i]=b[j];
+                b[j]=temp1;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=a[i]*b[i];
+    }
+    printf("%d\n",sum);
+    return 0;
+}
