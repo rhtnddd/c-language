2ce13ae8d4b24b358f324b98480039c5e3f79edb
성민규 <qlalfql123@gmail.com>
2025-08-24 22:08:45 +0900

    백준 9656문제

--- 내가 쓰는칸/백준 실버풀기.c
+++ 내가 쓰는칸/백준 실버풀기.c
@@ -82,12 +82,29 @@
 // }
 
 // 9655문제
+// #include <stdio.h>
+// int main(void)
+// {
+//     int n;
+//     scanf("%d",&n);
+//     if(n%2==1)
+//     {
+//         printf("SK\n");
+//     }
+//     else
+//     {
+//         printf("CY\n");
+//     }
+//     return 0;
+// }
+
+// 9656문제
 #include <stdio.h>
 int main(void)
 {
     int n;
     scanf("%d",&n);
-    if(n%2==1)
+    if(n%2==0)
     {
         printf("SK\n");
     }
