68339ef7f258f1be7e45a38537e399bf96efaf48
성민규 <qlalfql123@gmail.com>
2025-08-28 17:52:13 +0900

    코드수정

--- 내가 쓰는칸/ㅁㄴㅇ.c
+++ 내가 쓰는칸/ㅁㄴㅇ.c
@@ -18,10 +18,10 @@
 // {
 //     char n[101];
 //     int sum=0;
-//     scanf("%s",n);
+//     fgets(n,sizeof(n),stdin);
 //     for(int i=0;n[i]!='\0';i++)
 //     {
-//         if(n[i]!=',')
+//         if(48<=n[i]&&n[i]<=57)
 //         {
 //             sum+=n[i]-'0';
 //         }
@@ -58,31 +58,31 @@
 //     }
 // }
 // 4
-#include <stdio.h>
-int main(void)
-{
-    int n,m;
-    scanf("%d %d",&m,&n);
-    int num=1;
-    int a[101][101]={0};
-    for(int in=0;in<=m+n-2;in++)
-    {
-        for(int i=0;i<m;i++)
-        {
-            int j=in-i;
-            if(j>=0&&j<n)
-            {
-                a[j][i]=num++;
-            }
-        }
-    }
-    for(int i=0;i<n;i++)
-    {
-        for(int jn=0;jn<m;jn++)
-        {
-            printf("%d ",a[i][jn]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main(void)
+// {
+//     int n,m;
+//     scanf("%d %d",&m,&n);
+//     int num=1;
+//     int a[101][101]={0};
+//     for(int in=0;in<=m+n-2;in++)
+//     {
+//         for(int i=0;i<m;i++)
+//         {
+//             int j=in-i;
+//             if(j>=0&&j<n)
+//             {
+//                 a[j][i]=num++;
+//             }
+//         }
+//     }
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int jn=0;jn<m;jn++)
+//         {
+//             printf("%d ",a[i][jn]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
