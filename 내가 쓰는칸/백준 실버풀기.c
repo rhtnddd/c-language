56efbad875de984281e83d81b564982ada612b9f
성민규 <qlalfql123@gmail.com>
2025-09-04 18:02:29 +0900

    백준 21919문제

--- 내가 쓰는칸/백준 실버풀기.c
+++ 내가 쓰는칸/백준 실버풀기.c
@@ -480,39 +480,83 @@
 //         }
 //     }
 //     printf("%d\n",sum);
+//     return 0;
 // }
 
 // 4948문제
+// #include <stdio.h>
+// int a[3550000]={0,};
+// int main(void)
+// {
+//     int n=1;
+//     while(1)
+//     {
+//         int sum=0;
+//         scanf("%d",&n);
+//         if(n==0)
+//         {
+//             return 0;
+//         }
+//         for(int i=2;i*i<=n*2;i++)
+//         {
+//             if(!a[i])
+//             {
+//                 for(int j=i*i;j<=n*2;j+=i)
+//                 {
+//                    a[j]=1; 
+//                 }
+//             }
+//         }
+//         for(int i=n+1;i<=n*2;i++)
+//         {
+//             if(!a[i])
+//             {
+//                 sum+=1;
+//             }
+//         }
+//         printf("%d\n",sum);
+//     }
+//     return 0;
+// }
+
+// 21919문제
 #include <stdio.h>
-int a[3550000]={0,};
+int a[1000001];
+int used[1000001];
 int main(void)
 {
-    int n=1;
-    while(1)
+    int n,m[10001];
+    long long int sum=1;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
     {
-        int sum=0;
-        scanf("%d",&n);
-        if(n==0)
-        {
-            return 0;
-        }
-        for(int i=2;i*i<=n*2;i++)
-        {
-            if(!a[i])
-            {
-                for(int j=i*i;j<=n*2;j+=i)
-                {
-                   a[j]=1; 
-                }
-            }
-        }
-        for(int i=n+1;i<=n*2;i++)
-        {
-            if(!a[i])
-            {
-                sum+=1;
-            }
-        }
-        printf("%d\n",sum);
+        scanf("%d",&m[i]);
     }
+    for(int i=2;i*i<=1000000;i++)
+    {
+        if(!a[i])
+        {
+            for(int j=i*i;j<=1000000;j+=i)
+            {
+                a[j]=1; 
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(!a[m[i]]&&!used[m[i]])
+        {
+            sum*=m[i];
+            used[m[i]]=1;
+        }
+    }
+    if(sum!=1)
+    {
+        printf("%lld\n",sum);
+    }
+    else
+    {
+        printf("-1");
+    }
+    return 0;
 }
