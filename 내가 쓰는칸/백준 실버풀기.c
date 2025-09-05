3d28400e0fdc1a327051ddd1db66aa6a213a0b86
성민규 <qlalfql123@gmail.com>
2025-09-05 23:58:36 +0900

    백준16212문제

--- 내가 쓰는칸/백준 실버풀기.c
+++ 내가 쓰는칸/백준 실버풀기.c
@@ -520,43 +520,108 @@
 // }
 
 // 21919문제
+// #include <stdio.h>
+// int a[1000001];
+// int used[1000001];
+// int main(void)
+// {
+//     int n,m[10001];
+//     long long int sum=1;
+//     scanf("%d",&n);
+//     for(int i=0;i<n;i++)
+//     {
+//         scanf("%d",&m[i]);
+//     }
+//     for(int i=2;i*i<=1000000;i++)
+//     {
+//         if(!a[i])
+//         {
+//             for(int j=i*i;j<=1000000;j+=i)
+//             {
+//                 a[j]=1; 
+//             }
+//         }
+//     }
+//     for(int i=0;i<n;i++)
+//     {
+//         if(!a[m[i]]&&!used[m[i]])
+//         {
+//             sum*=m[i];
+//             used[m[i]]=1;
+//         }
+//     }
+//     if(sum!=1)
+//     {
+//         printf("%lld\n",sum);
+//     }
+//     else
+//     {
+//         printf("-1");
+//     }
+//     return 0;
+// }
+
+// 16212문제
 #include <stdio.h>
-int a[1000001];
-int used[1000001];
+int a[500001];
+int temp_a[500001];
+void merge(int *a, int start,int middle,int end)
+{
+    int i=start;
+    int j=middle+1;
+    int k=start;
+    while(i<=middle&&j<=end)
+    {
+        if(a[i]<=a[j])
+        {
+            temp_a[k]=a[i++];
+        }
+        else{
+            temp_a[k]=a[j++];
+        }
+        k++;
+    }
+    if(i>middle){
+        for(int t=j;t<=end;++t)
+        {
+            temp_a[k]=a[t];
+            ++k;
+        }
+    }
+	else{
+		for(int t=i;t<=middle;++t){
+			temp_a[k] = a[t];
+			++k;
+		}
+	}
+	for(int t=start;t<=end;++t){
+		a[t] = temp_a[t];
+	}
+}
+void mergeSort(int *a,int start,int end)
+{
+    if(start<end){
+        int middle=(start+end)/2;
+        mergeSort(a,start,middle);
+        mergeSort(a, middle+1, end);
+        merge(a, start, middle, end);
+    }
+}
 int main(void)
 {
-    int n,m[10001];
-    long long int sum=1;
+    int n;
     scanf("%d",&n);
     for(int i=0;i<n;i++)
     {
-        scanf("%d",&m[i]);
+        scanf("%d",&a[i]);
     }
-    for(int i=2;i*i<=1000000;i++)
-    {
-        if(!a[i])
-        {
-            for(int j=i*i;j<=1000000;j+=i)
-            {
-                a[j]=1; 
-            }
-        }
-    }
+    mergeSort(a,0,n-1);
     for(int i=0;i<n;i++)
     {
-        if(!a[m[i]]&&!used[m[i]])
+        printf("%d",a[i]);
+        if(i<n-1)
         {
-            sum*=m[i];
-            used[m[i]]=1;
+            printf(" ");
         }
     }
-    if(sum!=1)
-    {
-        printf("%lld\n",sum);
-    }
-    else
-    {
-        printf("-1");
-    }
-    return 0;
 }
