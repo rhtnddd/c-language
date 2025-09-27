3d82ca3f0811b7bdd839a20580834a586f448ec7
성민규 <qlalfql123@gmail.com>
2025-09-28 00:48:52 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 1152 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int n, m;
+int a[10000];
+void backtrack(int step, int start)
+{
+    if (step == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+        return;
+    }
+    else
+    {
+        for (int i = start; i <= n; i++)
+        {
+            a[step] = i;
+            backtrack(step + 1, i + 1);
+        }
+    }
+}
+int main(void)
+{
+    scanf("%d %d", &n, &m);
+    backtrack(0, 1);
+    return 0;
+}
