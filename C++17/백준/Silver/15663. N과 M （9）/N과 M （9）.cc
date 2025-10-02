6dc1ce149fe14172d70f5cb91e42c0d2bde53b33
성민규 <qlalfql123@gmail.com>
2025-10-03 01:19:26 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 1192 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int n, m;
+int a[10000];
+int b[10000];
+int check[10] = {0};
+void backtrack(int step)
+{
+    if (step == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            printf("%d ", a[i]);
+        }
+        printf("\n");
+        return;
+    }
+    else
+    {
+        int prev = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (check[i] == 0 && b[i] != prev)
+            {
+                prev = b[i];
+                check[i] = 1;
+                a[step] = b[i];
+                backtrack(step + 1);
+                check[i] = 0;
+            }
+        }
+    }
+}
+int main(void)
+{
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        for (int j = 0; j < n; j++)
+        {
+            if (b[i] < b[j])
+            {
+                temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    backtrack(0);
+    return 0;
+}
