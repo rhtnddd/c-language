aaa859e3783aca86b6a75d481278be3eda2e09ff
성민규 <qlalfql123@gmail.com>
2025-10-17 23:51:48 +0900

    [Bronze II] Title: 나누기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1075. 나누기/나누기.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int N, F;
+    scanf("%d", &N);
+    scanf("%d", &F);
+
+    N = N / 100 * 100;
+
+    for (int i = 0; i < 100; i++) {
+        if ((N + i) % F == 0) {
+            printf("%02d\n", i);
+            break;
+        }
+    }
+    return 0;
+}
