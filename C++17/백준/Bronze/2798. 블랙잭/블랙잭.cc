40cb8a6984c15b35aa2cd32b75477232c37238f1
성민규 <qlalfql123@gmail.com>
2025-10-30 23:49:40 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    int card[100];
+    int sum, max = 0;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) scanf("%d", &card[i]);
+    for (int i = 0; i < n - 2; i++) {
+        for (int j = i + 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                sum = card[i] + card[j] + card[k];
+                if (sum <= m && sum > max) max = sum;
+            }
+        }
+    }
+    printf("%d", max);
+    return 0;
+}
